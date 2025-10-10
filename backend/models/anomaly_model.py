7a767ca5404b5dd77d0ebc8cb78f33437bbfbc2e
Aleksandr Petrov <alexpetrov248@gmail.com>
2025-10-10 15:03:39 +0200

    Added fine-tuned model with 8 classes

--- backend/models/anomaly_model.py
+++ backend/models/anomaly_model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from config import ANOMALY_MODEL_PATH
 
 video_model = torch.hub.load("facebookresearch/pytorchvideo", "slow_r50", pretrained=False)
-video_model.blocks[-1].proj = nn.Linear(video_model.blocks[-1].proj.in_features, 1)
+video_model.blocks[-1].proj = nn.Linear(video_model.blocks[-1].proj.in_features, 8)
 
 checkpoint = torch.load(ANOMALY_MODEL_PATH, map_location="cpu")
 video_model.load_state_dict(checkpoint["model_state_dict"])
